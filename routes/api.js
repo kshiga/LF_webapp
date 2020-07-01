75c3a79323a5f6bec3fe8f61b1ef408ecbbe80f9
Kaitlyn Higa <kaitlynhiga@gmail.com>
2020-06-30 20:06:06 -0700

    Save message to db in api route

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var {EncodedMessage} = require('../models/encoded-message.js');
 
-router.post('/encode', function(req, res, next) {
-  var canEncode = true;
-  if(canEncode){
-    res.status(200).send({EncodedMessage: 'this is a Message'});
-  } else {
-    res.status(500).send({EncodedMessage: ''});
-  }
+router.post('/encode', async function(req, res, next) {
+  var encodedMessage = new EncodedMessage({ shift: req.body.shift, message: req.body.message });
+  encodedMessage.encode();
+  await encodedMessage.save().then((value) => {
+    if(value.encodedMessage.length){
+      res.status(200).send({EncodedMessage: value.encodedMessage});
+    } else {
+      res.status(500).send({EncodedMessage: ''});
+    }
+  });
 });
 
 module.exports = router;
--- test/routes/api_tests.js
+++ test/routes/api_tests.js
@@ -8,17 +8,50 @@ chai.should();
 describe("API", () => {
     describe("POST encode", () => {
       describe("encoding success", () => {
-        it("post should return with the encoded message", (done) => {
+        it("returns with a status code of 200", (done) => {
              chai.request(app)
                  .post('/api/encode')
+                 .send({ 'shift': 12, 'message': 'hello my name is not kaitlyn' })
                  .end((err, res) => {
                      res.should.have.status(200);
-                     res.body.should.be.a('object');
-                     res.body.should.have.property('EncodedMessage');
-                     res.body.EncodedMessage.should.equal('this is a Message');
                      done();
                   });
          });
+
+         it("returns the encoded message", (done) => {
+              chai.request(app)
+                  .post('/api/encode')
+                  .send({ 'shift': 12, 'message': 'hello my name is not kaitlyn' })
+                  .end((err, res) => {
+                      res.body.should.be.a('object');
+                      res.body.should.have.property('EncodedMessage');
+                      res.body.EncodedMessage.should.equal('tqxxa yk zmyq ue zaf wmufxkz');
+                      done();
+                   });
+          });
+      });
+      describe("encoding error", () => {
+        it("returns with a status code of 500", (done) => {
+             chai.request(app)
+                 .post('/api/encode')
+                 .send({ 'shift': 12, 'message': 'hello my name is not kaitlyn!' })
+                 .end((err, res) => {
+                     res.should.have.status(500);
+                     done();
+                  });
+         });
+
+         it("returns an empty string", (done) => {
+              chai.request(app)
+                  .post('/api/encode')
+                  .send({ 'shift': 12, 'message': 'hello my name is not kaitlyn!' })
+                  .end((err, res) => {
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('EncodedMessage');
+                    res.body.EncodedMessage.should.equal('');
+                      done();
+                   });
+          });
       });
 
     });
